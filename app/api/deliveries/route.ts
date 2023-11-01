caf80331a2f801b0772e7c3c32490dcca632846c
Dudu Lasry <davidlasry696@gmail.com>
2023-11-01 10:42:47 +0200

    Update route logic

--- app/api/deliveries/route.ts
+++ app/api/deliveries/route.ts
@@ -1,9 +1,52 @@
-import { db } from "@/app/db";
-import { Prisma } from "@prisma/client";
+import { db } from "@/src/db";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  const deliveries = await db.delivery.findMany();
+  const withCategory = {
+    include: {
+      category: true,
+    },
+  };
+
+  const withItem = {
+    include: {
+      item: withCategory,
+    },
+  };
+
+  const withAmounts = {
+    include: {
+      amounts: withItem,
+    },
+  };
+
+  const withOrders = {
+    include: {
+      orders: withAmounts,
+    },
+  };
+
+  const data = await db.delivery.findMany(withOrders);
+
+  const deliveries = data.map((delivery) => {
+    const amounts = delivery.orders.flatMap((o) => o.amounts);
+    const categories = {} as Record<string, any>;
+
+    for (const amount of amounts) {
+      const { name: categoryName } = amount.item.category;
+      const { name: itemName } = amount.item;
+
+      const category = categories[categoryName] || {};
+      const items = category[itemName] || [];
+      items.push(amount);
+
+      category[itemName] = items;
+      categories[categoryName] = category;
+    }
+
+    return { ...delivery, categories };
+  });
+
   return NextResponse.json({ deliveries });
 };
 
