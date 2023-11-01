92b398d1ca3ffed46b32e1224f172749c4ec305f
Dudu Lasry <davidlasry696@gmail.com>
2023-11-01 21:35:00 +0200

    Add SWR and basic calls

---
+++ src/api.ts
@@ -0,0 +1,24 @@
+import { Delivery, Order } from "@prisma/client";
+import axios, { AxiosResponse } from "axios";
+import useSWR from "swr";
+
+export const http = axios.create({ baseURL: "/api" });
+
+interface GetDeliveriesResponse {
+  deliveries: Delivery[];
+}
+
+interface CreateOrderResponse {
+  order: Order;
+}
+
+export const fetchDeliveries = async () => {
+  const { data } = await http.get<GetDeliveriesResponse>("/deliveries");
+  return data.deliveries;
+};
+export const useDeliveries = () => useSWR("/deliveries", fetchDeliveries);
+
+export const createOrder = async () => {
+  const { data } = await http.post<CreateOrderResponse>("/orders");
+  return data.order;
+};
